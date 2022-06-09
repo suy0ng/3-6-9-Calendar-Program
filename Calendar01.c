92678e16f9fe577f8eb1f304c574e55c314c51d5
suy0ng <103719872+suy0ng@users.noreply.github.com>
2022-06-09 11:08:05 +0900

    Update Calendar01.c

--- Calendar01.c
+++ Calendar01.c
@@ -6,12 +6,6 @@ enum Days {SUN,MON,TUE,WED,THU,FRI,SAT};
 
 int main()
 {
-	time_t timer;
-	struct tm* t;
-	timer = time(NULL); // 1970년 1월 1일 0시 0분 0초부터 시작하여 현재까지의 초
-	t = localtime(&timer); // 포맷팅을 위해 구조체에 넣기
-
-	
 	int mon = 6, j = 1,k = 1;
 	// 처음화면 출력
 	printf("                                                                                \n");
